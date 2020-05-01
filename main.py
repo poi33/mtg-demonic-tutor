da4a4c3440f0c9c52f439e608b3c85dde093f66f
Persijn Kwekkeboom <pkw@enonic.com>
2020-05-01 18:04:47 +0200

    work on the window layout

--- main.py
+++ main.py
@@ -8,10 +8,27 @@ with open('AllCards.json', 'r', encoding="utf-8") as f:
     columns = list(objects)
 
     if len(columns) > 0:
-        print (columns[0]['manaCost'])
-    
+        print(columns[0]['manaCost'])
+
 app = wx.App()
-frm = wx.Frame(None, title="Magic Card Finder")
-frm.show()
+frame = wx.Frame(None, title="Magic Card Finder")
+#panel = wx.Panel(frame)
+sizer = wx.GridSizer(0, 3, 10, 10)
+staticText = wx.StaticText(frame, -1, label="Card finder",
+                           style=wx.ALIGN_CENTRE_HORIZONTAL)
+moreText = wx.StaticText(frame, -1, label="second item")
+another = wx.StaticText(frame, -1, label="third item")
+
+sizer.Add(staticText)
+sizer.Add(moreText)
+sizer.Add(another)
+frame.SetSizer(sizer)
+
+font = staticText.GetFont()
+font.PointSize += 10
+staticText.SetFont(font)
+moreText.SetFont(font)
+another.SetFont(font)
+frame.Show()
 
 app.MainLoop()
